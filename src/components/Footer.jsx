e0cce254fd9162900f96aa2a225cf2f00206427f
sree-shankar <mmsreeshankar@gmail.com>
2024-09-18 15:01:55 +0530

    animate add -footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,14 @@ import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { keyframes } from '@emotion/react';
+
+// Define the keyframes for the animation
+const bounceAnimation = keyframes`
+  0% { transform: translateY(0); }
+  50% { transform: translateY(-5px); }
+  100% { transform: translateY(0); }
+`;
 
 
 
@@ -32,9 +40,13 @@ const Footer = () => {
 
                 <Box className="box-footer-2" mb={1}>
                     <Stack direction="row" spacing={2} alignItems={"center"}>
-                        <MailOutlineIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF" }} />
+                        <MailOutlineIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF",   
+                        '&.hovered': {   animation: `${bounceAnimation} 0.5s ease`,  }, }} className="icon-mail"/>
                         <a href="mailto:mmsreeshankar@gmail.com" style={{ textDecoration: 'none', color: 'inherit' }}>
-                            <Typography variant="h2" className='h2-variant'>
+                            <Typography variant="h2" className='h2-variant'
+                                 onMouseEnter={() => document.querySelector('.icon-mail').classList.add('hovered')}
+                                 onMouseLeave={() => document.querySelector('.icon-mail').classList.remove('hovered')}
+                            >
                                 mmsreeshankar@gmail.com
                             </Typography>
                         </a>
@@ -44,9 +56,13 @@ const Footer = () => {
 
                 <Box className="box-footer-2" mb={3} >
                     <Stack direction="row" spacing={2} alignItems={"center"}>
-                        <LocalPhoneOutlinedIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF" }} />
+                        <LocalPhoneOutlinedIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF",
+                            '&.hovered': {   animation: `${bounceAnimation} 0.5s ease`,  }, }} className="icon-phone"/>
                         <a href="tel:+919994904553" style={{ textDecoration: 'none', color: 'inherit' }}>
-                        <Typography variant="h2" className='h2-variant'>9994904553</Typography>
+                        <Typography variant="h2" className='h2-variant'
+                         onMouseEnter={() => document.querySelector('.icon-phone').classList.add('hovered')}
+                         onMouseLeave={() => document.querySelector('.icon-phone').classList.remove('hovered')}
+                        >9994904553</Typography>
                         </a>
                         <ContentCopyIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF" }} />
                     </Stack>
@@ -54,9 +70,13 @@ const Footer = () => {
 
                 <Box className="box-footer-2" mb={5}>
                    <Stack direction="row" spacing={2} alignItems={"center"}>
-                      <LinkedInIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF" }} />
+                      <LinkedInIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF",
+                      '&.hovered': {   animation: `${bounceAnimation} 0.5s ease`,  }, }} className="icon-linkedin"/>
                        <a href="https://www.linkedin.com/in/contact-sreeshankar/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none', color: 'inherit' }}>
-                       <Typography variant="h2" className='h2-variant'>LinkedIn</Typography>
+                       <Typography variant="h2" className='h2-variant'
+                          onMouseEnter={() => document.querySelector('.icon-linkedin').classList.add('hovered')}
+                          onMouseLeave={() => document.querySelector('.icon-linkedin').classList.remove('hovered')}
+                       >LinkedIn</Typography>
                        </a>
                        <ContentCopyIcon width={7} height={10} sx={{ color: theme?.palette?.mode === "dark" && "#FFFFFF" }} />
 
